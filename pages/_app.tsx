ab04abd766c47f2d0728c02c82e2b5cf19b8bc73
KotaMorikawa <koke.kokota0130@gmail.com>
2024-01-23 12:51:39 +0900

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import type { AppProps } from "next/app";
 import {
   ThirdwebProvider,
   embeddedWallet,
-  smartWallet,
+  metamaskWallet,
+  walletConnect,
 } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/navbar";
@@ -11,19 +12,33 @@ import Header from "../components/header";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
-      supportedWallets={[
-        smartWallet(embeddedWallet(), {
-          factoryAddress: "0x9838b534cd5950CB6ea9E7fa94c00CF3986F953B",
-          gasless: true,
-        }),
-      ]}
+      clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+      secretKey={process.env.NEXT_PUBLIC_SECRET_KEY}
+      supportedWallets={[embeddedWallet(), walletConnect(), metamaskWallet()]}
+      activeChain={{
+        chainId: 5040,
+        rpc: ["https://subnets.avax.network/onigiri/mainnet/rpc"],
+        nativeCurrency: {
+          decimals: 18,
+          name: "ONIGIRI MAINNET",
+          symbol: "ONGR",
+        },
+        icon: {
+          url: "/icon.png",
+          width: 100,
+          height: 100,
+          format: "png",
+        },
+        shortName: "onigiri",
+        slug: "onigiri",
+        testnet: false,
+        chain: "ONIGIRI",
+        name: "ONIGIRI Mainnet",
+      }}
     >
       <Header />
       <Component {...pageProps} />
